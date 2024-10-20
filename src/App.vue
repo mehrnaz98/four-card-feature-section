2c0ac2940b3b3e40fc0a33a36ff0b043e6e4a670
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-20 23:51:42 +0330

    Add header and main section

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1>Reliable, efficient delivery</h1>
+    <h2>Powered by Technology</h2>
+  </header>
+  <main>
+    <p>
+      Our Artificial Intelligence powered tools use millions of project data
+      points to ensure that your project is successful
+    </p>
+    <div>
+      <h2>Supervisor</h2>
+      <p>Monitors activity to identify project roadblocks</p>
+    </div>
+  </main>
+</template>
