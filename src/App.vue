c08c59916ccefdd4de0e9fd7f4689c4f55ade843
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-20 23:59:43 +0330

    Add background color and minimum height with the flex display for the main section

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <header>
-    <h1>Reliable, efficient delivery</h1>
-    <h2>Powered by Technology</h2>
-  </header>
-  <main>
-    <p>
-      Our Artificial Intelligence powered tools use millions of project data
-      points to ensure that your project is successful
-    </p>
-    <div>
-      <h2>Supervisor</h2>
-      <p>Monitors activity to identify project roadblocks</p>
-    </div>
-  </main>
+  <section
+    class="bg-[#FAFAFA] min-h-screen flex flex-col justify-center items-center"
+  >
+    <header>
+      <h1>Reliable, efficient delivery</h1>
+      <h2>Powered by Technology</h2>
+    </header>
+    <main>
+      <p>
+        Our Artificial Intelligence powered tools use millions of project data
+        points to ensure that your project is successful
+      </p>
+      <div>
+        <h2>Supervisor</h2>
+        <p>Monitors activity to identify project roadblocks</p>
+      </div>
+    </main>
+  </section>
 </template>
