dbb024c378a3cde9d5a762d2c84589965a5cad3a
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-21 23:33:26 +0330

    Add heading and description as named slots for h2 and p

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -3,9 +3,11 @@
     class="flex flex-col bg-white shadow-lg max-w-[21.5rem] rounded-lg p-7 border-t-4"
     :class="borderColor"
   >
-    <h2 class="font-bold text-[1.4rem] text-[#4C4E61]">Supervisor</h2>
+    <h2 class="font-bold text-[1.4rem] text-[#4C4E61]">
+      <slot name="heading">Default Heading</slot>
+    </h2>
     <p class="text-[0.875rem] text-[#A3A5AE] pt-3">
-      Monitors activity to identify project roadblocks
+      <slot name="description">Default description text</slot>
     </p>
     <div class="flex justify-end">
       <img
