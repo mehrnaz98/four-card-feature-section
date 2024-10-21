aa840ea176bd60b8f89456da89f8cc77d7bd54a6
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-21 23:32:31 +0330

    Define a dynamic class for the border color

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,6 +1,7 @@
 <template>
   <div
-    class="flex flex-col bg-white shadow-lg max-w-[21.5rem] border-t-4 border-[#45D3D3] rounded-lg p-7"
+    class="flex flex-col bg-white shadow-lg max-w-[21.5rem] rounded-lg p-7 border-t-4"
+    :class="borderColor"
   >
     <h2 class="font-bold text-[1.4rem] text-[#4C4E61]">Supervisor</h2>
     <p class="text-[0.875rem] text-[#A3A5AE] pt-3">
