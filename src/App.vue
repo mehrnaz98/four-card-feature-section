230be14b2de18d75bb76f1e36473e201c41fa48d
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-21 23:22:01 +0330

    Move base card into BaseCard component

--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,7 @@
         Our Artificial Intelligence powered tools use millions of project data
         points to ensure that your project is successful
       </p>
-      <div
-        class="flex flex-col bg-white shadow-lg max-w-[21.5rem] border-t-4 border-[#45D3D3] rounded-lg p-7"
-      >
-        <h2 class="font-bold text-[1.4rem] text-[#4C4E61]">Supervisor</h2>
-        <p class="text-[0.875rem] text-[#A3A5AE] pt-3">
-          Monitors activity to identify project roadblocks
-        </p>
-        <div class="flex justify-end">
-          <img
-            src="./assets/images/icon-supervisor.svg"
-            alt=""
-            class="pt-8 max-w-16"
-          />
-        </div>
-      </div>
+      <BaseCard />
     </main>
   </section>
 </template>
