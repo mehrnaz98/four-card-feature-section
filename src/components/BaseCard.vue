0a7145b3ad377fb5ae65194d2b4f0edde84b9366
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-22 21:35:47 +0330

    Apply poppins font

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -3,10 +3,10 @@
     class="flex flex-col bg-white shadow-lg max-w-[21.5rem] rounded-lg p-7 border-t-4"
     :class="borderColor"
   >
-    <h2 class="font-bold text-[1.4rem] text-[#4C4E61]">
+    <h2 class="poppins-semibold text-[1.15rem] text-[#4C4E61]">
       <slot name="heading">Default Heading</slot>
     </h2>
-    <p class="text-[0.875rem] text-[#A3A5AE] pt-3">
+    <p class="poppins-regular text-[0.85rem] text-[#A3A5AE] pt-2">
       <slot name="description">Default description text</slot>
     </p>
     <div class="flex justify-end">
