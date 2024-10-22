845b4f6d3d1305b9d25efd9564cfc1cf9d258c01
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-22 21:20:53 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <section
     class="bg-[#FAFAFA] min-h-screen flex flex-col justify-center items-center p-6"
   >
-    <header class="text-[2.2rem]">
-      <h1 class="font-light">Reliable, efficient delivery</h1>
-      <h2 class="font-semibold-bold">Powered by Technology</h2>
+    <header class="text-[2rem]">
+      <h1 class="poppins-extralight">Reliable, efficient delivery</h1>
+      <h2 class="poppins-semibold">Powered by Technology</h2>
     </header>
     <main class="flex flex-col justify-center items-center">
       <div class="w-[30rem]">
